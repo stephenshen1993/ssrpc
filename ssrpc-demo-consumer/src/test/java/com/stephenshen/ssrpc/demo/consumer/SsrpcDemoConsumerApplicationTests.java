c0c5be88152d1bdf9aa228508d6dc5d501ec11bd
stephenshen1993 <15810813994@163.com>
2024-04-06 21:21:06 +0800

    update consumer application test case

--- ssrpc-demo-consumer/src/test/java/com/stephenshen/ssrpc/demo/consumer/SsrpcDemoConsumerApplicationTests.java
+++ ssrpc-demo-consumer/src/test/java/com/stephenshen/ssrpc/demo/consumer/SsrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,29 @@
 package com.stephenshen.ssrpc.demo.consumer;
 
+import com.stephenshen.ssrpc.demo.provider.SsrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class SsrpcDemoConsumerApplicationTests {
 
+    static  ConfigurableApplicationContext context;
+
+    @BeforeAll
+    static void init() {
+        context = SpringApplication.run(SsrpcDemoProviderApplication.class, "--server.port=8084", "loggging.level.com.stephenshen.ssrpc=debugg");
+    }
     @Test
     void contextLoads() {
+        System.out.println(" ===> aaa .... ");
     }
 
+    @AfterAll
+    static void destroy() {
+        SpringApplication.exit(context, () -> 1);
+    }
 }
